43f45b0269ff8c51dd8828d155d74a1fc9a27131
lorencd23 <lorencd23@gmail.com>
2023-01-31 13:06:32 +0100

    MVP-SIMPSON-01

---
+++ MVP-SIMPSON-01/app/src/main/java/com/example/mvp_simpson_01/lst_simpson/model/pojo/Simpson.java
@@ -0,0 +1,16 @@
+package com.example.mvp_simpson_01.lst_simpson.model.pojo;
+
+public class Simpson {
+    /*
+    * "quote": "Last night's 'Itchy and Scratchy' was, without a doubt, the worst episode ever. Rest assured that I was on the Internet within minutes, registering my disgust throughout the world.",
+        "character": "Comic Book Guy",
+        "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FComicBookGuy.png?1497567511970",
+        "characterDirection": "Right"
+    * */
+    //private static final String QUOTE = "QUOTE"
+    private String quote;
+    private String character;
+    private String image;
+    private String characterDirection;
+
+}
